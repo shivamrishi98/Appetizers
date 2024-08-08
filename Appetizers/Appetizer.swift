d91a093694ad2d937106783addd7a9000f8f223a
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-08 21:45:45 +1000

    Setup Appetizer Model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,39 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Shivam Rishi on 8/8/2024.
+//
+
+import Foundation
+
+// https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse: Decodable {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
