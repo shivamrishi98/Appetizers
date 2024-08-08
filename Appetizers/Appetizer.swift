79e9a1f7f6e50f9720cfa022615ac2e5e0ced031
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-08 22:07:06 +1000

    Created AppetizerListCell

--- Appetizers/Appetizer.swift
+++ Appetizers/Appetizer.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/
 
-struct Appetizer: Decodable {
+struct Appetizer: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
@@ -30,7 +30,7 @@ struct MockData {
                                            name: "Test Appetizer",
                                            description: "This is the description for my appetizer. It's yummy.",
                                            price: 9.99,
-                                           imageURL: "",
+                                           imageURL: "test",
                                            calories: 99,
                                            protein: 99,
                                            carbs: 99)
