2cfb0455496cf9ee0ca5f3517a730d017f324470
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-09 16:02:13 +1000

    Created Loading View

---
+++ Appetizers/LoadingView.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by Shivam Rishi on 9/8/2024.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = UIColor(.brandPrimary)
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea(.all)
+            ActivityIndicator()
+        }
+    }
+}
