0865f484b9c2ae6840df612549fb37e1f57cbfd0
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-13 18:45:40 +1000

    Created Order UI

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle(Text("🧾 Orders"))
+            VStack {
+                List {
+                    ForEach(orderItems){ appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle(Text("🧾 Orders"))
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
