28c77d497a348ed023a39eb14700cdc04be6da41
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-14 14:06:15 +1000

    Use Environment object for Order

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -9,39 +9,35 @@ import SwiftUI
 
 struct OrderView: View {
     
-    @State private var orderItems = MockData.orderItems
+    @EnvironmentObject var order: Order
     
     var body: some View {
         NavigationView {
             ZStack {
                 VStack {
                     List {
-                        ForEach(orderItems){ appetizer in
+                        ForEach(order.items){ appetizer in
                             AppetizerListCell(appetizer: appetizer)
                         }
-                        .onDelete(perform: deleteItems)
+                        .onDelete(perform: order.deleteItems)
                     }
                     .listStyle(.plain)
                     
                     Button {
                         print("tapped")
                     } label: {
-                        APButton(title: "$99.99 - Place Order")
+                        APButton(title: "$\(order.totalPrice, specifier: "%.2f") - Place Order")
                     }
                     .padding(.bottom, 25)
                 }
                 
-                if orderItems.isEmpty {
+                if order.items.isEmpty {
                     EmptyState(imageName: "empty-order",
                                message: "You have no items in your order.\nPlease add an appetizer!")
                 }
             }
             .navigationTitle(Text("🧾 Orders"))
         }
-    }
-    
-    func deleteItems(at offsets: IndexSet) {
-        orderItems.remove(atOffsets: offsets)
     }
 }
 
